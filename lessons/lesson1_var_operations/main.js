38293c05efcddc7756b9db5e3501071c31667f68
GrayHead <ss.zhuravlov@gmail.com>
2024-08-05 20:30:15 +0300

    lesson 1

---
+++ lessons/lesson1_var_operations/main.js
@@ -0,0 +1,107 @@
+
+// 'use strict';
+
+// // alert('hello okten');
+// let glass = 100500;
+// glass = 200600;
+//
+//
+// // {
+// //     let a = 100;
+// //     var b = 200;
+// // }
+// //
+// // // console.log(a);
+// // console.log(b);
+//
+//
+// const PI = 3.14;
+// console.log(PI);
+
+
+// //     number
+// let num1 = 1004000;
+// //     string
+//
+// let word1 = 'hello';
+// word1 = "hello";
+// word1 = `hello`;
+//
+// //     boolean
+// let bool1 = true;
+// bool1 = false;
+//
+// // 4 способи
+//
+// // let x;
+// // let x = undefined;
+// let x = null;
+// console.log(x);
+// // 5 + базові допоміжні
+
+
+// operations +++
+
+// nums====
+
+// let a = 10;
+// let b = 20;
+//
+// let result = a + b + 100500;
+// console.log(result);
+// console.log(10 % 5);
+
+// strings====
+// let word1 = 'hello';
+// let word2 = 'okten';
+// let word3 = '!!!';
+// let space = ' ';
+//
+// let result = word1 + space + word2 + space + word3;
+// console.log(result);
+//
+// let result2 = `${word1} ${word2} ${word3}`;
+// console.log(result2);
+
+// let x = null;
+// console.log(typeof x);
+
+// let bool1 = true; // 1
+// let bool2 = false; // 0
+// console.log(bool1 + bool2);
+// console.log(100 / false);
+// console.log(100 + true);
+
+
+// convertation ++++++
+
+// let str1 = '1000'; // string
+// console.log(typeof +str1);
+//
+// let num1 = 2000;
+// console.log(typeof (num1 + ''));
+// console.log(num1 + '');
+//
+// // console.log(!!1000);
+// // console.log(!!' ');
+//
+// console.log(!!0);
+// console.log(!!'');
+// console.log(!!undefined);
+// console.log(!!null);
+// console.log(!!NaN);
+//
+// console.log(!!+'0');
+//
+// console.log(100 + 200 + '');
+// console.log('' + 100 + 200);
+//
+// let data = !!'sad';
+// console.log(data);
+// console.log(data);
+
+
+let answer = prompt();
+console.log(!!answer);
+
+// let msg = `${wor1} ${word2}`;
