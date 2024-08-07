18c5240b1d9cb2346f378856af6fbf24682bb801
GrayHead <ss.zhuravlov@gmail.com>
2024-08-07 20:26:29 +0300

    lesson 2 obj arrays if switch

---
+++ lessons/lesson2/main.js
@@ -0,0 +1,98 @@
+// let array = [123, 'asd', true];
+// array[0] = 234;
+// array[100] = 'okten';
+// console.log(array);
+// console.log(array.length);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[65]);
+
+
+// let names = [
+//     'vasya',
+//     'petya',
+//     'olya',
+//     'taras',
+//     ['tom', 'jerry', 'spike']
+// ];
+// let namesOfCharacters = names[4];
+// console.log(namesOfCharacters);
+// console.log(namesOfCharacters[1]);
+// console.log(names[4][1]);
+
+// let newVar = [];
+
+
+// let dog = {
+//     name: 'fluffy',
+//     age: 2,
+//     status: true,
+//     commands: ['sit', 'bark', 'lay', 'get over here']
+// }
+//
+// console.log(dog);
+// console.log(dog.name);
+// console.log(dog['name']);
+// console.log(dog.commands[2]);
+//
+// dog.breed = 'spaniel';
+// // dog['breed'] = 'spaniel';
+// console.log(dog);
+//
+// delete dog.age;
+//
+// console.log(dog);
+//
+//
+// let users = [
+//     {username: 'kokos', surname: 'kokosiv', age: 321, skills: ['hrml', 'js']},
+//     {username: 'abrikos', surname: 'abrikosiv', age: 2},
+//     {username: 'yabluchko', surname: 'tomatne', age: 4},
+// ]
+//
+// console.log(users[2].age);
+// console.log(users[users.length - 1].age);
+// console.log(users[0].skills[1]);
+// console.log(users[2]);
+
+// let user = {
+//     id: 1,
+//     name: 'vasya',
+//     age: 31,
+//     wife: {
+//         id: 123,
+//         name: 'anna'
+//     }
+// }
+//
+// console.log(user.wife);
+// console.log(user.wife.name);
+// console.log(user.wife.id);
+// console.log(user.name);
+// console.log(user.wife.name);
+
+
+// let a = 0;
+// let b = a+1;
+// console.log(a, b);
+
+
+// let user1 = {id: 1}
+// let user2 = user1
+//
+// console.log(user1);
+// console.log(user2);
+// user2.id = 2;
+// console.log(user2);
+// console.log(user1,'!!!');
+
+// const obj = {}
+// obj.id = 100500;
+// console.log(obj);
+
+
+
+
+
+
+
